96eb718e899a13acbbe0cf520078051388d8e092
ThaiDuong2083 <duong0964036886@gmail.com>
2025-07-13 16:50:02 +0700

    dark mode

---
+++ lesson/module3/day4/call-api/src/enum.jsx
@@ -0,0 +1,15 @@
+const PathVariable = Object.freeze({
+    ABOUT_ME: '/about-me',
+    HOME_PAGE: '/',
+    NEWS: '/news',
+    ORDER: '/order',
+    MENU: '/menu',
+    PROFILE: '/user-profile/profile',
+    SETTING_ADDRESS: '/user-profile/setting-address',
+    COUPON: '/user-profile/coupons',
+    LOGOUT: '/logout',
+    SIGN_IN: '/sign-in',
+    SIGN_UP: '/sign-up',
+});
+
+export default PathVariable;
